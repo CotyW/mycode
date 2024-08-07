177092d07a9c41981009d3b123831cb9e74569bf
CotyW <cwoodsid@visa.com>
2024-08-07 21:40:15 +0000

    learning parse log files

---
+++ farmloop.py
@@ -0,0 +1,14 @@
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+
+
+
+#loop for animals
+
+for farm in farms:
+    print("-", farm["name"])
+    choice = input("Pick a farm!\n")
+
+
