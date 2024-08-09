1e7ccd629c09b191eb250b671e81328ae6e709e1
CotyW <cwoodsid@visa.com>
2024-08-09 14:08:12 +0000

    RPG Project

---
+++ 1GameProject/roomdescriptions.py
@@ -0,0 +1,10 @@
+# listing descriptions here to make the rooms file not so lengthy.  
+descriptions = {
+    'Hall': 'A long, narrow hallway with a creaky wooden floor.',
+    'Great Hall': 'The Great Hall is an expansive, majestic chamber with towering stone walls and a high vaulted ceiling.',
+    'Kitchen': 'A modern kitchen with shiny countertops and a fridge full of leftovers.',
+    'Dining Room': 'A grand dining room with a large oak table and a crystal chandelier.',
+    'Garden': 'A lush garden with blooming flowers and a small fountain.',
+    'Courtyard': 'An open courtyard with a weathered basketball hoop on one side.',
+    'cupboard': 'You look into the cupboard to see a brand new basketball',
+}
---
+++ 1GameProject/rooms.py
@@ -0,0 +1,47 @@
+from roomdescriptions import descriptions 
+
+rooms = {
+    'Hall': {
+        'north': 'Great Hall',
+        'south': 'Kitchen',
+        'east': 'Dining Room',
+        'item': 'key',
+        'description': descriptions['Hall'],
+    },
+    'Great Hall': {
+        'south': 'Hall',
+        'item': '',
+        'description': descriptions['Great Hall']
+    },
+    'Kitchen': {
+        'north': 'Hall',
+        'item': 'cupboard',
+        'cupboard': 'cupboard',
+        'description': descriptions['Kitchen'],
+    },
+    'cupboard':{
+        'item': 'basketball',
+        'kitchen': 'Kitchen',
+        'description': descriptions['cupboard'],
+    },
+    'Dining Room': {
+        'west': 'Hall',
+        'south': 'Garden',
+        'description': descriptions['Dining Room'],
+    },
+    'Garden': {
+        'north': 'Dining Room',
+        'east': 'Courtyard',
+        'west': 'Stables',
+        'description': descriptions['Garden'],
+    },
+    'Stables': {
+        'east': 'Garden',
+
+    },
+    'Courtyard': {
+        'west': 'Garden',
+        'item': 'basketball hoop',
+        'description': descriptions['Courtyard'],
+    }
+}
